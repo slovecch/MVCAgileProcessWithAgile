f0ac347bf06ef158d03c92705b6bd9e4100902c5
Stefan Lovecchio <slovecch@my.centennialcollege.ca>
2024-11-09 13:31:11 -0500

    Connect the database #222

--- MVCAgileProcessWithAgile/Program.cs
+++ MVCAgileProcessWithAgile/Program.cs
@@ -1,9 +1,18 @@
+using MVCAgileProcessWithAgile.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
